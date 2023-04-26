35792b2df4cdf1a09451e0cb171d05e9696795d6
mschu <chuminsung@gmail.com>
2023-04-26 08:05:18 +0000

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
 import express from "express";
 
-const PORT = 4000;
+/////////////////////////////////////////////////////
+// express init
+/////////////////////////////////////////////////////
+const PORT = 3000;
 const app = express();
+
+/////////////////////////////////////////////////////
+// express configure
+/////////////////////////////////////////////////////
+const handlerHome = (req, res) => {
+    console.log("route : ",req.method,req.url);
+    return res.send("Youtube Clone Homepage");
+};
+app.get("/", handlerHome);
+
+const handleLogin = (req, res) => {
+    console.log("route : ",req.method,req.url);
+    return res.send("Youtube Clone Login Page");
+}
+app.get("/login", handleLogin);
+
+/////////////////////////////////////////////////////
+// express listen
+/////////////////////////////////////////////////////
 const handleListening = () => 
     console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
