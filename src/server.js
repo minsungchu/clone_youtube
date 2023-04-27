cfa8a886f6a8bc806b14bcd8a553b5c06a69b4f7
mschu <chuminsung@gmail.com>
2023-04-27 02:18:37 +0000

    #3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,28 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 /////////////////////////////////////////////////////
 // express init
 /////////////////////////////////////////////////////
 const PORT = 3000;
 const app = express();
+const logger = morgan("dev");
 
 /////////////////////////////////////////////////////
 // express configure
 /////////////////////////////////////////////////////
-const logger  = (req, res, next) => {
-    console.log(`logger > ${req.method} ${req.url}`);
-    next();
-}
-app.use(logger);
-
 const handlerHome = (req, res) => {
     return res.send("Youtube Clone Homepage");
 };
-app.get("/", handlerHome);
-
 const handleLogin = (req, res) => {
     return res.send("Youtube Clone Login Page");
 }
+app.use(logger);
+app.get("/", handlerHome);
 app.get("/login", handleLogin);
 
 /////////////////////////////////////////////////////
